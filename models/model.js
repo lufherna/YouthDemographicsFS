36fd0d5e03232f4d9576123011033741e9a00ad0
lufherna <rojoclifford@gmail.com>
2017-07-06 01:59:22 -0400

    added additional code to the model.js file and others

---
+++ models/model.js
@@ -0,0 +1,68 @@
+var misc = require('../controllers/controller.js');
+// console.log('this is connected to ' + router)
+var misc1 = require('../server.js');
+var express = require('express');
+var app = express();
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+	host	: "localhost",
+	user	: "root",
+	password: "root",
+	database: "happiness_chart"
+}); // end of connection to mySQL database
+
+// test the connection to the sql database
+connection.connect(function(err) {
+	if(err) {
+		console.log('error connecting ' + err);
+		return;
+	}
+
+	console.log('connected as id ' + connection.threadId);
+}); // end of connection test 
+
+// below was used to test out the connection 
+// we now know that the connection is coming back with data
+connection.query('SELECT * FROM happiness_stats', function(err, data) {
+	if (err) {
+		throw err
+	}
+
+	console.log('first test of the data: ', data);
+})
+
+// shows the sql data in object notation
+app.get('/', function(req, res) {
+	connection.query('SELECT * FROM happiness_stats;', function(err, data) {
+		if (err) {
+			throw err
+		}
+		// res.render('random info', { happiness_stats: data});
+		// not getting a response back, not sure why
+		res.render( { happiness_stats: data})
+	});
+});
+
+// allows users to insert data into sql database
+app.post('/', function(req, res) {
+	connection.query('INSERT INTO happiness_stats (stats) VALUES (?)', [req.body.stats],
+		function(err, result) {
+			if (err) {
+				throw err
+			}
+			res.redirect('/')
+		};
+	});
+
+// allows user to delete 
+app.post('/delete', function(req, res) {
+	connection.query('DELETE FROM stats WHERE id = ?', [req.body.id],
+		function(err, result) {
+			if(err) {
+				throw err;
+			}
+
+			res.redirect('/')
+		});
+	});
